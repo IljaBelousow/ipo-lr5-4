919a303aa2cbf486d91df758743ad9c66319bf2b
ПИКМИ МЕН <103372208+IljaBelousow@users.noreply.github.com>
2024-12-21 10:59:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-kolvo = int(input("vvedite kolvo slov "))
+kolvo = int(input("введите количество слов"))
 a = []
 b = ""
 for i in range(kolvo):#выводит слова и доабвчялет в a[]
-    slovo = str(input("vashe slovo "))
+    slovo = str(input("ваше слово "))
     a += [slovo]
 string = [x[0] for x in a]#первая буква добавялется в b""
 for i in string:
